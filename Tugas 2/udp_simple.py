d19bda97458a608c2d41f7b2d4a41dd01970fe03
awinawin1 <43516997+awinawin1@users.noreply.github.com>
2020-03-01 15:25:54 +0700

    Update udp_simple.py

--- Tugas 2/udp_simple.py
+++ Tugas 2/udp_simple.py
@@ -1,5 +1,5 @@
 import socket
 TARGET_IP = "10.151.253.224"
-TARGET_PORT = 5005
+TARGET_PORT = 5006
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.sendto(bytes('ABCDEFGHIJKLMNOPQRSTUV'.encode()),(TARGET_IP,TARGET_PORT))
