4f52511aebdd4c1af5836503f3861b3976538090
awinawin1 <43516997+awinawin1@users.noreply.github.com>
2020-03-01 15:26:33 +0700

    Update udpfileclient.py

--- Tugas 2/udpfileclient.py
+++ Tugas 2/udpfileclient.py
@@ -1,7 +1,7 @@
 import socket
 import os
 TARGET_IP = "10.151.253.224"
-TARGET_PORT = 5005
+TARGET_PORT = 5006
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 namafile="bart.png"
 ukuran = os.stat(namafile).st_size
